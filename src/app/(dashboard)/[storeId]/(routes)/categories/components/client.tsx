52542a3a840f6155c2350074f8c62efc6b688665
som-bit <sr8180525@gmail.com>
2023-11-20 21:26:24 +0530

    added categories field (not completed)

---
+++ src/app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { Button } from "@/components/ui/Button";
+import { Heading } from "@/components/ui/Heading";
+import { Separator } from "@/components/ui/Separator";
+import { DataTable } from "@/components/ui/data-table"
+
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { CategoryColumn, columns } from "./columns"
+import { ApiList } from "@/components/ui/api-list";
+
+// interface to add data to billboard to modify the heading and manage billboard
+//and add more div in the billboard page
+
+interface CategoryClientProps {
+    data: CategoryColumn[]
+}
+
+const CategoryClient: React.FC<CategoryClientProps> = ({
+    data
+}) => {
+    const params = useParams();
+    const router = useRouter();
+
+    return (
+        <>
+            <div className=" flex items-center justify-between">
+                <Heading
+                    title={`Categories (${data.length})`}
+                    description="Manage Categories for your store"
+                />
+                <Button onClick={() => router.push(`/${params.storeId}/Categories/new`)}>
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add new
+                </Button>
+            </div>
+            <Separator />
+            {/*the the data table component that is shown below*/}
+            <DataTable columns={columns} data={data} searchKey="name" />
+            <Heading title="API" description="Api Calls for Categories" />
+            <Separator />
+            <ApiList  entityName="categories" entityIdName="categoryId" />
+        </>
+    );
+}
+
+export default CategoryClient;
